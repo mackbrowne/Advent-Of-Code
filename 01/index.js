32aef626f162c5fcd72349d45c258f083e4c3e81
Mack <mackbrowne@Kristens-MacBook-Pro-2.local>
2019-12-01 10:42:50 +0100

    erase 2017 to prepare for 2019

--- 01/index.js
+++ 01/index.js
@@ -1,24 +1,2 @@
 const fs = require('fs');
 const inputFile = fs.readFileSync('input.txt').toString();
-
-const findDoubles = (input, nextDigit) => {
-    let sum = 0;
-    for(let i = 0; i<input.length; i++){
-        const a = input[i];
-        let b;
-
-        if(i < input.length - nextDigit){
-            b = input[i + nextDigit];
-        }else{
-            b = input[i + nextDigit - input.length];
-        }
-
-        if(a === b){
-            sum += parseInt(a);
-        }
-    }
-    return sum;
-};
-
-console.log(`A - ${findDoubles(inputFile, 1)}`);
-console.log(`B - ${findDoubles(inputFile, inputFile.length / 2)}`);
