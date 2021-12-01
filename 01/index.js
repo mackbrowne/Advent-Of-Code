700fcab3c476fac7e0a5f32a49ee60be06c76bca
mackenziebrowne-wk <mackenzie.browne@workiva.com>
2021-12-01 12:41:42 +0100

    Finish 2021-01

--- 01/index.js
+++ 01/index.js
@@ -1,22 +1,35 @@
-const { parseNumbers } = require(`../utils/file`);
+const { parseNumbers, readFile } = require(`../utils/file`);
 const { test, endTest } = require(`../utils/test`);
-const { sum, multiply } = require("../utils/reducers");
-const { findCombos } = require("../utils/list");
+const { sum } = require("../utils/reducers");
 
 const parseInput = (file) => parseNumbers(`${__dirname}/${file}`, "\n");
 
 const test1 = parseInput("test1.txt");
 const input = parseInput("input.txt");
 
-const sum2020 = (combo) => sum(combo) === 2020;
+const group3Windows = (values) =>
+  values.reduce(
+    (windowGroups, currentVal, index, array) => {
+      if(index <= array.length - 2) windowGroups.push(sum([currentVal, array[index + 1], array[index + 2]]));
+      return windowGroups;
+    },
+    []
+  );
 
-const run = async () => {
-  test(multiply(await findCombos(test1, 2, sum2020)), 514579);
-  test(multiply(await findCombos(test1, 3, sum2020)), 241861950);
+const countIncrease = (list) =>
+  list.reduce(
+    (result, currentVal, index, array) =>
+      index > 0 && currentVal > array[index - 1] ? result + 1 : result,
+    0
+  );
+
+const run = () => {
+  test(countIncrease(test1), 7);
+  test(countIncrease(group3Windows(test1)), 5);
   endTest();
 
-  console.log(`Part 1 - ${multiply(await findCombos(input, 2, sum2020))}`);
-  console.log(`Part 2 - ${multiply(await findCombos(input, 3, sum2020))}`);
+  console.log(`Part 1 - ${countIncrease(input)}`);
+  console.log(`Part 2 - ${countIncrease(group3Windows(input))}`);
 };
 
 run();
