89a0e2015a02d46e20b1ad0948aebc5fae1cbced
mackenziebrowne-wk <mackenzie.browne@workiva.com>
2021-12-15 18:20:23 +0100

    WIP shopify interview

--- 07/index.js
+++ 07/index.js
@@ -1,103 +1,80 @@
 const { parseNumbers, readFile } = require(`../utils/file`);
 const { test, endTest } = require(`../utils/test`);
-const { sum, multiply } = require("../utils/reducers");
 
-const parseInput = (file) => readFile(`${__dirname}/${file}`, "\n");
+class LRU {
+  constructor(size) {
+    if(size <= 0) throw Error('size must be greater than 0');
+    this.size = size;
+    this.keys = [];
+    this.cache = {};
+  }
 
-const test1 = parseInput("test1.txt");
-const input = parseInput("input.txt");
-
-const score = (board) =>
-  sum(
-    board
-      .flat(2)
-      .filter((letter) => letter !== "X")
-      .map(Number)
-  );
-
-const checkBingo = (board) => {
-  const SIZE = board.length; //5
-
-  const isBingo = (rowCol) =>
-    rowCol.filter((item) => item === "X").length === rowCol.length;
-
-  for (let i = 0; i < SIZE; i += SIZE) {
-    console.log(i);
-    const row = board[i];
-    const col = board.map((row) => row[i]);
-    console.log(col);
-    if (isBingo(row) || isBingo(col)) {
-      console.log("BINGO");
-      //return true;
+  read(key){
+    //find the key if it exist
+    const returnValue = this.cache[key];
+    if(!returnValue){
+      return undefined;
     }
+
+    //update the usage cache keys
+    //remove item from keys
+    const [poppedValue] = this.keys.splice(this.keys.indexOf(key), 1);
+
+    //push to the end
+    this.keys.push(poppedValue);
+
+    //return value
+    return returnValue;
   }
 
-  return false;
-};
+  write(key, value){
+    if(!this.read(key)){
+      this.keys.push(key);
+      if(this.count() > this.size) this.delete(this.keys[0])
+    } 
 
-const part1 = (input) => {
-  const [calledNumbersLine, , ...boardLines] = input;
-
-  const calledNumbers = calledNumbersLine.split(",");
-
-  const boards = boardLines.reduce(
-    (acc, line) => {
-      if (line.length > 0) {
-        let lastBoard = acc.pop();
-        const lineNumbers = line
-          .match(/\d+ */g)
-          .map((matchedLine) => matchedLine.trim());
-        lastBoard.push(lineNumbers);
-        acc.push(lastBoard);
-      } else {
-        acc.push(new Array());
-      }
-
-      return acc;
-    },
-    [[]]
-  );
-
-  //play bingo
-  try {
-    calledNumbers.forEach((pickedNumber) => {
-      console.log(pickedNumber);
-      // console.log(boards);
-      boards.forEach((board, boardIndex) => {
-        // console.log(`board ${index + 1}`);
-        // console.log(board);
-        board.forEach((boardLine, lineIndex) => {
-          let foundIndex = boardLine.indexOf(pickedNumber);
-          if (foundIndex >= 0) {
-            boardLine[foundIndex] = "X";
-            if (!!checkBingo(board)) {
-              throw { board, lastPicked: pickedNumber };
-            }
-          }
-        });
-      });
-    });
-  } catch ({ board, lastPicked }) {
-    return [score(board), Number(lastPicked)];
+    this.cache[key] = value;
   }
-};
 
-const part2 = (input) => {};
+  delete(key){
+    console.log('deleting ' + key)
+    delete this.cache[key];
+    this.keys.splice(this.keys.indexOf(key), 1);
+  }
 
-const run = () => {
-  const [boardScore, lastPicked] = part1(test1);
-  test(boardScore, 188);
-  test(lastPicked, 24);
-  test(multiply([boardScore, lastPicked]), 4512);
+  count(){
+    return this.keys.length;
+  }
 
-  // const part2Result = part2(test1);
-  // test(part2Result[0], 23);
-  // test(part2Result[1], 10);
-  // test(part2Result[0] * part2Result[1], 230);
-  endTest();
+  clear(){
+    this.cache = {};
+    this.keys = [];
+  }
+}
 
-  console.log(`Part 1 - ${multiply(part1(input))}`);
-  // console.log(`Part 2 - ${multiply(part2(input))}`);
-};
 
-run();
+const lru = new LRU(5);
+
+lru.read('hi');
+lru.write('one', 1);
+lru.write('two', 2);
+lru.write('three', 3);
+lru.write('four', 4);
+lru.write('five', 5);
+lru.read('one');
+lru.write('six', 6);
+
+console.log(lru.keys);
+console.log(lru.cache);
+
+console.log(lru.read('hi'));
+
+lru.read('another');
+
+lru.delete('one');
+
+console.log(lru.keys);
+console.log(lru.cache);
+
+console.log(lru.count());
+console.log(lru.clear());
